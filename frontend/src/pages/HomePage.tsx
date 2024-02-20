2382440930f94184b404328a565f2f323dab0311
NisipeanuIonut <ionut.nisipeanu@caelus.ro>
2024-02-20 16:35:00 +0200

    Functional Frontend
    Added the functional frontend + hierarchy

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage!</h1>
+            {/* Additional content goes here */}
+        </div>
+    );
+};
+
+export default HomePage;
